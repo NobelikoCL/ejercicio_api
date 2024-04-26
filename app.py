d38ba12711bdee0c9d29a177b7fce20defbdde2f
Hernan Villanueva <104547024+NobelikoCL@users.noreply.github.com>
2024-04-25 23:29:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def bird_gallery():
+    page = request.args.get('page', 1, type=int)
     url = "https://aves.ninjas.cl/api/birds"
     response = requests.get(url)
     birds = response.json() if response.status_code == 200 else []
-    return render_template('template.html', birds=birds)
+    return render_template('template.html', birds=birds, page=page)
 
 if __name__ == '__main__':
     app.run(debug=True)
